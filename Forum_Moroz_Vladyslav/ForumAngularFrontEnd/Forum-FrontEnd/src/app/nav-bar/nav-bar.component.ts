4515eb31c6c88f5dd322c173f18007cfeb33cc09
vladm747 <vladyslav.moroz105@gmail.com>
2022-08-18 14:11:07 +0300

    add Manage Users to a navbar

--- Forum_Moroz_Vladyslav/ForumAngularFrontEnd/Forum-FrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ Forum_Moroz_Vladyslav/ForumAngularFrontEnd/Forum-FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Emitters } from '../emmiters/emmiters';
+import { ForumApiService } from '../forum-api.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,20 +10,50 @@ import { Emitters } from '../emmiters/emmiters';
 })
 export class NavBarComponent implements OnInit {
   authenticated = false;
+  isAdmin: boolean;
+  currentUser: any;
   constructor(
+    private service: ForumApiService,
     private http: HttpClient
+
   ) { }
 
   ngOnInit(): void {
     Emitters.authEmitter.subscribe(
       (auth: boolean) => {
-        this.authenticated = auth; 
+        this.authenticated = auth;
       }
     );
+    this.service.getCurrentUser().subscribe((res:any) => {
+      this.currentUser = res;
+    },
+    (err:any) => {
+      this.currentUser = null;
+      console.log(err);
+    });
+
+  }
+
+  isAdministrator(): boolean {
+    this.isAdmin = false;
+
+    if(this.currentUser==null)
+    {
+      this.isAdmin = false;
+    }
+    else if(this.currentUser.roles.includes('admin'))
+    {
+      this.isAdmin = true;
+    }
+
+    return this.isAdmin;
   }
 
   logOut(): void {
     this.http.post('https://localhost:44394/api/Auth/SignOut', {}, {withCredentials: true})
-      .subscribe(() => this.authenticated = false);
+      .subscribe(() => {
+        this.authenticated = false;
+        this.isAdmin = false;
+      });
   }
 }
